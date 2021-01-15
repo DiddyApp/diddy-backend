2a3f7303bbf902474209c1a412c0e7a01809fd59
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2021-01-15 16:24:41 +0200

    Add mock functions

--- src/lambdas/Goals/DeleteGoalFunction.cs
+++ src/lambdas/Goals/DeleteGoalFunction.cs
@@ -3,16 +3,16 @@ using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 
-namespace Authentication
+namespace Goals
 {
-    public class AttachIdentityFunction
+    public class DeleteGoalFunction
     {
         public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
             return new APIGatewayProxyResponse
             {
                 StatusCode = 200,
-                Body = JsonConvert.SerializeObject(new { Message = "Identity attached." }),
+                Body = JsonConvert.SerializeObject(new { Message = "Goal deleted." }),
                 Headers = new Dictionary<string, string>
                 {
                     { "Content-Type", "application/json" },
--- src/lambdas/Goals/GetGoalFunction.cs
+++ src/lambdas/Goals/GetGoalFunction.cs
@@ -3,16 +3,16 @@ using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 
-namespace Authentication
+namespace Goals
 {
-    public class AttachIdentityFunction
+    public class GetGoalFunction
     {
         public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
             return new APIGatewayProxyResponse
             {
                 StatusCode = 200,
-                Body = JsonConvert.SerializeObject(new { Message = "Identity attached." }),
+                Body = JsonConvert.SerializeObject(new { Message = "Here's your goal." }),
                 Headers = new Dictionary<string, string>
                 {
                     { "Content-Type", "application/json" },
