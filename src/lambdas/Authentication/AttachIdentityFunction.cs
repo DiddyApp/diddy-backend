d6cf0f284a35f8ca170605d8ac151c3bf05b40e6
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2021-01-15 16:19:34 +0200

    Change projects to .NET Core

---
+++ src/lambdas/Authentication/AttachIdentityFunction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+namespace Authentication
+{
+    public class AttachIdentityFunction
+    {
+        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Body = JsonConvert.SerializeObject(new { Message = "Account created." }),
+                Headers = new Dictionary<string, string>
+                {
+                    { "Content-Type", "application/json" },
+                    { "Access-Control-Allow-Origin", "*" }
+                }
+            };
+        }
+    }
+}
