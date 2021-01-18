04d0d89049b9ae97d0301b2508f252d913e0d1c6
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2021-01-18 14:22:34 +0200

    Add code for CreateAccount, dynamo and cognito

---
+++ src/infrastructure/AuthenticationConstruct.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using Amazon.CDK;
+using Amazon.CDK.AWS.APIGateway;
+using Amazon.CDK.AWS.Cognito;
+using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.Lambda;
+
+namespace Infrastructure
+{
+    public class AuthenticationConstruct : Construct
+    {
+        public AuthenticationConstruct(Construct scope, string id) : base(scope, id)
+        {
+            var userPool = new UserPool(scope, "UserPool");
+            var userPoolClient = new UserPoolClient(scope, "UserPoolClient",
+                new UserPoolClientProps
+                {
+                    UserPoolClientName = "User Pool Client",
+                    UserPool = userPool,
+                    GenerateSecret = false,
+                    PreventUserExistenceErrors = true,
+                    AuthFlows = new AuthFlow
+                    {
+                        UserPassword = true,
+                        AdminUserPassword = true,
+                        UserSrp = true,
+                        Custom = true
+                    }
+                });
+            var usersTable = new Table(scope, "UsersTable",
+                new TableProps
+                {
+                    TableName = "Users",
+                    BillingMode = BillingMode.PAY_PER_REQUEST, // will probably be changes to Provisioned in production
+                    PartitionKey = new Attribute
+                    {
+                        Name = "id",
+                        Type = AttributeType.STRING
+                    },
+                });
+
+            var createAccountFunction = new Function(scope, "CreateAccount", new FunctionProps
+            {
+                Runtime = Runtime.DOTNET_CORE_3_1,
+                Code = Code.FromAsset("../lambdas/authentication/publish"),
+                Handler = "Authentication::Authentication.CreateAccountFunction::FunctionHandler"
+            });
+
+            usersTable.GrantReadWriteData(createAccountFunction);
+            createAccountFunction.AddToRolePolicy(new Amazon.CDK.AWS.IAM.PolicyStatement(new PolicyStatementProps
+            {
+                Effect = Effect.ALLOW,
+                Actions = new[] { "cognito-idp:InitiateAuth" },
+                Resources = new[] { userPool.UserPoolArn },
+            }));
+
+            //TODO: code for the AttachIdentity function; needs more research
+
+            var api = new RestApi(this, "Authentication-API", new RestApiProps
+            {
+                RestApiName = "Authentication Service",
+            });
+
+            var createAccountIntegration = new LambdaIntegration(createAccountFunction, new LambdaIntegrationOptions
+            {
+                RequestTemplates = new Dictionary<string, string>
+                {
+                    ["application/json"] = "{ \"statusCode\": \"200\" }"
+                },
+            });
+
+            api.Root.AddMethod("GET", createAccountIntegration);
+        }
+    }
+}
