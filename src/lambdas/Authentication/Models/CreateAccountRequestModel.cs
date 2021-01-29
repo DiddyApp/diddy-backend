28f3ffb190111a7c4d9cbde644f2428e8486e97b
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2021-01-29 16:13:23 +0200

    Create CreateAccount lambda function

---
+++ src/lambdas/Authentication/Models/CreateAccountRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Authentication.Models
+{
+    public class CreateAccountRequestModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ src/lambdas/Authentication/Models/CreateAccountResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Authentication.Models
+{
+    public class CreateAccountResponseModel
+    {
+        public string UserId { get; set; }
+
+        public string IdToken { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+}
