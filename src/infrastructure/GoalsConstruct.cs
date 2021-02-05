cf728e067054abe1d39f26c2f43aa131a50a4192
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2021-02-05 11:21:24 +0200

    Add permission

--- src/infrastructure/GoalsConstruct.cs
+++ src/infrastructure/GoalsConstruct.cs
@@ -22,7 +22,7 @@ namespace Infrastructure
             });
             var apiGatewayResources = new ApiGatewayResources(scope, id, apiParent, lambdaResources, userPool);
 
-            dynamoDb.GoalsTable.Grant(lambdaResources.AddGoal, new string[] { "dynamodb:DescribeTable", "dynamodb:PutItem" });
+            dynamoDb.GoalsTable.Grant(lambdaResources.AddGoal, new string[] { "dynamodb:DescribeTable", "dynamodb:PutItem", "dynamodb:UpdateItem" });
             dynamoDb.GoalsTable.Grant(lambdaResources.GetGoal, new string[] { "dynamodb:DescribeTable", "dynamodb:GetItem", "dynamodb:Query" });
             dynamoDb.GoalsTable.GrantReadWriteData(lambdaResources.DeleteGoal);
             dynamoDb.GoalsTable.GrantReadWriteData(lambdaResources.UpdateGoal);
