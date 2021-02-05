c34ed8023d83a1bf137cee3a76632cf655972b9a
arjunraghurama <arjunr0019@gmail.com>
2021-02-05 21:48:01 +0530

    UpdateGoal Lambda
    Added implimentation for UpdateGoal Lambda function

---
+++ src/lambdas/Goals/Models/DeltaGoal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goals.Models
+{
+    public class DeltaGoal
+    {
+        public long Delta { get; set; }
+    }
+}
--- src/lambdas/Goals/UpdateCounterFunction.cs
+++ src/lambdas/Goals/UpdateCounterFunction.cs
@@ -1,24 +1,55 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Common.LambdaUtils;
+using Goals.Models;
 using Newtonsoft.Json;
 
 namespace Goals
 {
     public class UpdateCounterFunction
     {
-        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        public static string _goalsTableName = Environment.GetEnvironmentVariable("GOALS_TABLE_NAME");
+        public static DynamoDBContext _dynamoDbContext = null;
+
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            return new APIGatewayProxyResponse
+            try
             {
-                StatusCode = 200,
-                Body = JsonConvert.SerializeObject(new { Message = "Counter updated." }),
-                Headers = new Dictionary<string, string>
+                // Get the UID from the JWT
+                request.RequestContext.Authorizer.Claims.TryGetValue("cognito:username", out string uid);
+
+                // Load table globally for optimization, because of the DescribeTable call
+                if (_dynamoDbContext == null)
                 {
-                    { "Content-Type", "application/json" },
-                    { "Access-Control-Allow-Origin", "*" }
+                    var client = new AmazonDynamoDBClient();
+                    _dynamoDbContext = new DynamoDBContext(client);
                 }
-            };
+
+                // Deserialize the request
+                var deltaGoal = JsonConvert.DeserializeObject<DeltaGoal>(request.Body);
+
+                // Get the current Goal value stored in DB
+                var currentGoalState = await _dynamoDbContext.LoadAsync<Goal>(uid);
+
+                // Update the counter value
+                currentGoalState.Counter += deltaGoal.Delta;
+
+                // Save the updated value to DB
+                await _dynamoDbContext.SaveAsync(currentGoalState);
+
+                // Return the updated Goal 
+                return new APIGatewayProxyResponse().CreateSuccessResponse(currentGoalState);
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log(e.ToString());
+                return new APIGatewayProxyResponse().CreateErrorResponse(e.ToString());
+            }
         }
     }
 }
