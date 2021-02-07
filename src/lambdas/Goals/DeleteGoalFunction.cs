ff5e4f6697f39e5825f0f02b22a4da7ec7b4a72d
arjunraghurama <arjunr0019@gmail.com>
2021-02-07 22:50:31 +0530

    Implementation for DeleteGoal

--- src/lambdas/Goals/DeleteGoalFunction.cs
+++ src/lambdas/Goals/DeleteGoalFunction.cs
@@ -1,24 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Common.LambdaUtils;
+using Goals.Models;
 using Newtonsoft.Json;
 
 namespace Goals
 {
     public class DeleteGoalFunction
     {
-        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        public static string _goalsTableName = Environment.GetEnvironmentVariable("GOALS_TABLE_NAME");
+        public static DynamoDBContext _dynamoDbContext = null;
+
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            return new APIGatewayProxyResponse
+            try
             {
-                StatusCode = 200,
-                Body = JsonConvert.SerializeObject(new { Message = "Goal deleted." }),
-                Headers = new Dictionary<string, string>
+                // Get the UID from the JWT
+                request.RequestContext.Authorizer.Claims.TryGetValue("cognito:username", out string uid);
+
+                // Load table globally for optimization, because of the DescribeTable call
+                if (_dynamoDbContext == null)
                 {
-                    { "Content-Type", "application/json" },
-                    { "Access-Control-Allow-Origin", "*" }
+                    var client = new AmazonDynamoDBClient();
+                    _dynamoDbContext = new DynamoDBContext(client);
                 }
-            };
+
+                // Get the GoalId from Query
+                var goalId = request.QueryStringParameters["id"];
+
+                // Delete item using HashKey and RangeKey
+                await _dynamoDbContext.DeleteAsync<Goal>(uid, goalId);
+
+                // Return response
+                return new APIGatewayProxyResponse().CreateSuccessResponse("Delete Successful");
+
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log(e.ToString());
+                return new APIGatewayProxyResponse().CreateErrorResponse(e.ToString());
+            }
         }
     }
 }
